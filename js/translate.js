ca72cff92022be1d20abde85f481dcb727120f0f
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2022-12-27 16:07:34 +0100

    Now MegaTrivia is multilingual and added some options

---
+++ js/translate.js
@@ -0,0 +1,92 @@
+class Translate {
+
+  constructor (lang) {
+    if (!lang || typeof lang !== 'string') {
+      console.error('Lang no puede estar vacía o no es una cadena');
+      return false;
+    }
+    this.lang = lang
+    this.fetchTranslation();
+  }
+
+  fetchTranslation() {
+    if (typeof fetch == 'undefined') {
+      var request = new XMLHttpRequest();
+
+      request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+          this.translation = JSON.parse(this.response);
+          this.translateHtml();
+        } else console.error(this.status, this.statusText);
+      };
+
+      request.onerror = function () {
+        console.error(this.status, this.statusText);
+      };
+
+      request.send();
+    } else {
+      fetch(`/i18n/${this.lang}.json`)
+        .then((res) => res.json())
+        .then((translation) => {
+          this.translation = translation;
+          this.translateHtml();
+        })
+        .catch(() => {
+          console.error(`Could not load ${this.lang}.json.`);
+        });
+    }
+  }
+
+  changeLang(lang, cb = null) {
+    if (!lang || typeof lang !== 'string') {
+      console.error('Lang no puede estar vacía o no es una cadena');
+      return false;
+    }
+    this.lang = lang
+    this.fetchTranslation();
+
+    if (typeof cb == 'function') cb(this.translation);
+  }
+
+  /**
+   * Translate all tags with data-i18n attribute
+   *
+   * @memberof Translate
+   */
+  translateHtml() {
+    const elements = document.querySelectorAll('[data-i18n]');
+    elements.forEach((ele) => {
+      const trans = ele.getAttribute('data-i18n');
+      if (trans in this.translation) ele.innerHTML = this.translation[trans];
+      else ele.innerHTML = trans;
+    });
+  }
+
+  __(key, replaces = null) {
+    if (!key || typeof key !== 'string') {
+      return false
+    }
+
+    if (key in this.translation) {
+      let message = this.translation[key];
+
+      if (replaces && typeof replaces == 'object') {
+        return Object.keys(replaces).reduce(
+          (text, key) =>
+            text.replace(
+              new RegExp(`{\s*${key}\s*}`, "g"),
+              replaces[key],
+            ),
+          message,
+        );
+      } else {
+        return this.translation[key];
+      }
+    }
+
+    return key;
+  }
+}
+
+export default Translate;
--- js/trivia.js
+++ js/trivia.js
@@ -1,3 +1,5 @@
+import Translate from "./translate.js";
+
 let corrects = 0;
 let failed = 0;
 let ids = [0, 1, 2, 3];
@@ -6,9 +8,29 @@ let questions;
 let seconds = 20;
 let trivials;
 let trivia_name;
-let trivia_file;
 let questions_timer = null;
 let block_answers = false;
+let options = {};
+const nav_lang = navigator.language.slice(0, 2);
+const langs_supported = ['es', 'en'];
+
+// En el caso de que en el navegador se este usando los idiomas co-oficiales (Catalán, Euskera y Gallego)
+// usaremos por defecto el español, al menos hasta que las traducciones estén disponibles
+if (nav_lang == 'ca' || nav_lang == 'eu' || nav_lang == 'gl')
+  nav_lang = 'es'
+
+if (localStorage.getItem('MegaTriviaConf') == null) {
+  options = {
+    lang: (langs_supported.includes(nav_lang)) ? nav_lang : 'en',
+    time: 20
+  }
+
+  localStorage.setItem('MegaTriviaConf', JSON.stringify(options));
+} else {
+  options = JSON.parse(localStorage.getItem('MegaTriviaConf'));
+}
+
+const translate = new Translate(options.lang);
 
 $().get('trivials.json', function(resp) {
   trivials = resp.trivials;
@@ -38,6 +60,9 @@ $('.show-page').on('click', function() {
     case 'top-ratings':
       showRecords();
       break;
+    case 'options':
+      loadConf();
+      break;
   
     default:
       break;
@@ -48,7 +73,7 @@ $('.show-page').on('click', function() {
 function showtrivials() {
   $('#trivia_selection').empty();
   trivials.forEach(function (trivia, i) {
-    const button = `<button class="btn-green trivia" data-trivia="${i}">${trivia.name}</button>`;
+    const button = `<button class="btn-green trivia" data-trivia="${i}"><img src="flags/${trivia.lang}.svg" /> ${trivia.name}</button>`;
     $('#trivia_selection').append(button);
   });
 }
@@ -65,24 +90,25 @@ $('#trivia_selection').on('click', '.trivia', function(e) {
     $('#trivia').addClass('active');
     corrects = 0;
     failed = 0;
-    seconds = 20;
+    seconds = options.time;
     current_question = 0;
   }
 });
 
 // Comprobamos si hay una nueva puntuación máxima de un trivial. Si no es así solo guarda la puntuación si se acertó al menos 3 preguntas
 function setMaxScores() {
-  if (window.localStorage.getItem('MegaTrivia') == null && corrects >= 3){
-    punt = [{
+  $('#new-record').hide();
+  if (localStorage.getItem('MegaTrivia') == null && corrects >= 3) {
+    const punt = [{
       name: trivia_name,
       corrects: corrects
     }];
     
-    window.localStorage.setItem('MegaTrivia', JSON.stringify(punt));
-    $('#gameover .modal-body').append('<br /><b>Ha obtenido un nuevo récord</b>');
+    localStorage.setItem('MegaTrivia', JSON.stringify(punt));
+    $('#new-record').show();
   } else {
-    const scores = JSON.parse(window.localStorage.getItem("MegaTrivia"));
-    result = scores.filter((data) => {
+    const scores = JSON.parse(localStorage.getItem("MegaTrivia"));
+    let result = scores.filter((data) => {
       return (data.name == trivia_name)
     });
 
@@ -90,28 +116,28 @@ function setMaxScores() {
       scores.forEach(function(item) {
         if (item.name == trivia_name && item.corrects < corrects){
           item.corrects = corrects;
-          $('#gameover .page-body').append('<br /><b>Ha obtenido un nuevo récord</b>');
+          $('#new-record').show();
         }
       });
     } else {
-      $('#gameover .page-body').append('<br /><b>Ha obtenido un nuevo récord</b>');
+      $('#new-record').show();
       scores.push({"name": trivia_name, "corrects": corrects});
     }
 
-    window.localStorage.setItem("MegaTrivia", JSON.stringify(scores));
+    localStorage.setItem("MegaTrivia", JSON.stringify(scores));
   }
 }
 
 // Muestra las puntuaciones máximas
 function showRecords() {
   $('#best-scores > tbody').empty();
-  scores = JSON.parse(window.localStorage.getItem("MegaTrivia"));
+  const scores = JSON.parse(localStorage.getItem("MegaTrivia"));
   if (scores != null) {
     scores.forEach(function(item) {
-      $('#best-scores > tbody').append(`<tr><td>${item.name}</td><td>${item.corrects}</td></tr>`);
+      $('#best-scores > tbody').append(`<tr><td>${item.name}</td><td class="score-points">${item.corrects}</td></tr>`);
     });
   } else {
-    $('#best-scores > tbody').html('<tr><td colspan="2">Aun no hay ninguna puntuación guardada</td></tr>');
+    $('#best-scores > tbody').html('<tr><td colspan="2" id="no-records">' + translate.__('no_records') + '</td></tr>');
   }
 }
 
@@ -119,20 +145,20 @@ function showRecords() {
 function newQuestion() {
   block_answers = false;
   if (current_question <= questions.length - 1 && failed < 3) {
-    seconds = 20;
+    seconds = options.time;
     $('#cur-time').text(seconds);
     $('#trivia-answers').empty();
     ids = shuffleArray(ids);
-    var item = questions[current_question];
+    const item = questions[current_question];
     $('#trivia-question').html(item.title);
 
     for (let i = 0; i < 4; i++) {
-      li_str = `<button class="btn-blue question" idresp="${ids[i]}">${item.answers[ids[i]]}</button>`;
+      const li_str = `<button class="btn-blue question" idresp="${ids[i]}">${item.answers[ids[i]]}</button>`;
       $('#trivia-answers').append(li_str);
     }
 
     current_question++;
-    $('#cur-question > span').text(current_question);
+    $('#cur-question').text(translate.__('question-n', { cur_question: current_question }));
     $('#acer-total').text(`${corrects} / ${questions.length}`);
 
     if (questions_timer) clearInterval(questions_timer);
@@ -150,8 +176,7 @@ function newQuestion() {
     }, 1000);
   } else {
     clearInterval(questions_timer);
-    $('#total-correct').text(`${corrects}`);
-    $('#total-failed').text(`${failed}`);
+    $('#game-over-text').html(translate.__('game_over', { total_correct: corrects, total_failed: failed }));
     $('.page').removeClass('active');
     $('#gameover').addClass('active');
     setMaxScores()
@@ -183,4 +208,23 @@ $('#trivia').on('click', '.question', function() {
 
   block_answers = true;
   setTimeout(newQuestion, 1000);
+});
+
+function loadConf() {
+  $('#sel-lang').val(options.lang);
+  $(`#sel-lang > option`).removeAttr('selected');
+  $(`#sel-lang > option[value=${options.lang}]`).attr('selected', true);
+  $(`#sel-time > option`).removeAttr('selected');
+  $(`#sel-time > option[value=${options.time}]`).attr('selected', true);
+}
+
+$('#save-options').on('click', function() {
+  const lang = $('#sel-lang').val();
+  const time = $('#sel-time').val();
+
+  translate.changeLang(lang);
+  options.lang = lang;
+  options.time = time;
+
+  localStorage.setItem('MegaTriviaConf', JSON.stringify(options));
 });
