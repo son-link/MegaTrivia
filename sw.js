e7d3737d296803e4d7a497328a9b71dd4e293eba
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2023-07-01 14:33:27 +0200

    Update year and STATIC_CACHE_VERSION

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@ const CACHE_LIST = [
   "/flags/en.svg",
   "/flags/es.svg"
 ];
-const STATIC_CACHE_VERSION = `static-v1-221230200000`;
+const STATIC_CACHE_VERSION = `static-v1-23070100000`;
 const DEBUG = false;
 
 self.addEventListener('install', function(event) {
